3852aa4ddcbe41bca0bb54e12b83ac0f97115673
Juan Andres Nuñez Lopez <nunezlopez.ja@gmail.com>
2023-03-21 17:31:42 -0600

    Made some progress on drawing multiple squares. Apparently now I have a better formula for calculating the number and sizes of horizontal squares

--- examples/multiple_squares.cpp
+++ examples/multiple_squares.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <string>
 #include <vector>
 
@@ -8,47 +9,53 @@
 int main(void)
 {
     // Initialization
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+    const int screenWidth = 1000;
+    const int screenHeight = 1000;
 
     int n_squares;
-
-    Rectangle boxA = {
-    20,
-    screenHeight - ((float)screenHeight / 2),
-    40,
-    40 };
-
     std::cout << "Input number of squares: ";
     std::cin >> n_squares;
     std::cout << "Squares: " << n_squares << "\n";
 
+    float size_squares = floor(screenWidth / (n_squares + 1));
+    float padding = (screenWidth - (size_squares * n_squares)) / (n_squares + 1);
 
-    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - Drawing multiple squares");
+    Rectangle boxA = {
+    padding,
+    screenHeight - ((float)screenHeight / 2),
+    size_squares,
+    size_squares};
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-
-    int x_incr = boxA.width + 20;
-    int y_incr = boxA.height + 20;
+    float x_incr = boxA.width + padding;
+    int y_incr = boxA.height + 30;
 
     // Main game loop
+    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - Drawing multiple squares");
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // TODO: Update your variables here
 
-        if (IsKeyPressed(KEY_SPACE)) {
-            if (boxA.x + x_incr <= screenWidth - 20) {
-                boxA.x += x_incr;
-            }
-            else {
-                boxA.x = 20;
-            }
+        // if (IsKeyPressed(KEY_SPACE)) {
+        //     if (boxA.x + x_incr <= screenWidth - padding) {
+        //         boxA.x += x_incr;
+        //     }
+        //     else {
+        //         boxA.x = padding;
+        //     }
+        // }
+
+        if (boxA.x + x_incr <= screenWidth - padding) {
+            boxA.x += x_incr;
+        }
+        else {
+            boxA.x = padding;
         }
 
         // Draw
         BeginDrawing();
 
-            // ClearBackground(RAYWHITE);
+            ClearBackground(RAYWHITE);
 
             // DrawRectangle(screenWidth/2 - 128, screenHeight/2 - 128, 256, 256, BLACK);
             // DrawRectangle(screenWidth/2 - 112, screenHeight/2 - 112, 224, 224, RAYWHITE);
