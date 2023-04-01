d9241e3da1accee16f7822c7026833630bc09926
Juan Andres Nuñez Lopez <nunezlopez.ja@gmail.com>
2023-04-01 12:18:37 -0600

    Managed to draw squares from top left to bot right, depending on the width and height of the screen. Updating its 'x' and 'y' positions and using a Rectangle struct

--- examples/multiple_squares.cpp
+++ examples/multiple_squares.cpp
@@ -18,16 +18,17 @@ int main(void)
     std::cout << "Squares: " << n_squares << "\n";
 
     float size_squares = floor(screenWidth / (n_squares + 1));
-    float padding = (screenWidth - (size_squares * n_squares)) / (n_squares + 1);
+    float x_padding = (screenWidth - (size_squares * n_squares)) / (n_squares + 1);
+    float y_padding = (screenHeight - (size_squares * n_squares)) / (n_squares + 1);
 
     Rectangle boxA = {
-    padding,
-    screenHeight - ((float)screenHeight / 2),
-    size_squares,
-    size_squares};
+        x_padding,
+        y_padding,
+        size_squares,
+        size_squares };
 
-    float x_incr = boxA.width + padding;
-    int y_incr = boxA.height + 30;
+    float x_incr = boxA.width + x_padding;
+    int y_incr = boxA.height + y_padding;
 
     // Main game loop
     InitWindow(screenWidth, screenHeight, "raylib [shapes] example - Drawing multiple squares");
@@ -36,25 +37,22 @@ int main(void)
     {
         // TODO: Update your variables here
 
-        // if (IsKeyPressed(KEY_SPACE)) {
-        //     if (boxA.x + x_incr <= screenWidth - padding) {
-        //         boxA.x += x_incr;
-        //     }
-        //     else {
-        //         boxA.x = padding;
-        //     }
-        // }
-
-        if (boxA.x + x_incr <= screenWidth - padding) {
+        if (boxA.x + x_incr <= screenWidth - x_padding) {
             boxA.x += x_incr;
         }
         else {
-            boxA.x = padding;
+            boxA.x = x_padding;
+
+            if (boxA.y + y_padding + y_incr >= screenHeight - y_padding) {
+                boxA.y = y_padding;
+            }
+            else {
+                boxA.y += y_incr;
+            }
         }
 
         // Draw
         BeginDrawing();
-
             ClearBackground(RAYWHITE);
 
             // DrawRectangle(screenWidth/2 - 128, screenHeight/2 - 128, 256, 256, BLACK);
@@ -63,7 +61,6 @@ int main(void)
             DrawRectangleRec(boxA, GOLD);
 
             DrawText("Press SPACEBAR to continue.", 300, screenHeight - 20, 15, GRAY);
-
         EndDrawing();
     }
 
